7fbf505f1dda0c91e00c0e8404fc61a16216e636
RoyaIScar <85313243+RoyaIScar@users.noreply.github.com>
2022-07-23 08:04:59 +0100

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -5,10 +5,11 @@ this autofarm just snakes peoples loot since how they made rendering trinkets
 honestly the devs for this game are so shitty releasing a game that doesnt look finished at all and literally no anti exploit at all
 --]]
 
+local dead = false
 local event = game.ReplicatedStorage.Events.DataEvent
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-2865.08813, 456.738953, -1090.94714, 1, 0, 0, 0, 1, 0, 0, 0, 1)
-wait(0.5)
+task.wait(0.5)
 game.Players.LocalPlayer.Character.HumanoidRootPart.RootJoint:Destroy()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CanCollide = true
 function grabtrinkets(trinkets)
@@ -18,7 +19,22 @@ end
 game.ReplicatedStorage.ChildAdded:connect(function(ae)
 if ae:IsA("MeshPart") and ae.Name ~= "Peach" and ae.Name ~= "Apple" and ae.Name ~= "Orange" and ae.Name ~= "Pear" and ae.Name ~= "Mango" and ae.Name ~= "Banana" then
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = ae.CFrame
-    repeat wait() until ae:FindFirstChild("ID")
+    repeat task.wait() until ae:FindFirstChild("ID")
 grabtrinkets(ae.ID.Value)
     end
 end)
+
+task.spawn(function()
+while task.wait() do
+    if not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") or game.Players.LocalPlayer.Character.Humanoid.Health == 0 then
+        dead = true
+       end
+    end
+end)
+
+repeat task.wait()
+if dead == true then
+    game.Players.LocalPlayer:Kick("niga died somehow")
+
+end
+until dead == true
